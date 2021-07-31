a9a29016b4cd1d9737c6bf55646886dadcc5e888
arcanlte <pinangayjoel@gmail.com>
2021-07-31 13:50:33 -0400

    Dynamic routes have been learned

--- things.js
+++ things.js
@@ -11,5 +11,18 @@ router.post("/", function (req, res) {
   res.send("POST route on thigns");
 });
 
+//DYNAMIC routes
+router.get("/:id", function (req, res) {
+  res.send("The id you specified is " + req.params.id);
+});
+
+router.get("/:id/:name", function (req, res) {
+  res.send("id: " + req.params.id + " name:" + req.params.name);
+});
+
+router.get("*", function (req, res) {
+  res.send("Sorry, this is an invalid URL.");
+});
+
 //2. export this router to use in our index.js
 module.exports = router;
